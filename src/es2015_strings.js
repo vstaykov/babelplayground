0a8242ea66541f6bf0c8a95e696f74088b074134
Ventsislav Staykov <vencislav.staykov@gmail.com>
2018-04-11 17:06:13 +0300

    Add ES 2015 strings example

---
+++ src/es2015_strings.js
@@ -0,0 +1,6 @@
+(function () {
+    var message = "hello";
+
+    console.log(`"hello".includes("llo") => ${message.includes("llo")}`);
+    console.log(`"hello".includes("LlO") => ${message.includes("LlO")}`);
+})();
